dba640d3a3499120a5b2885673085cf41ae7a0e0
Zach Burkhardt <zburkhardt18@gmail.com>
2018-09-25 22:48:45 -0400

    Added global variables for later use

--- main.cpp
+++ main.cpp
@@ -4,8 +4,20 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+const int HELICOPTER = 130,
+          TURBO_PROP = 250,
+          MIDSIZE_JET = 400,
+          HEAVY_JET = 550;
+
+const float CLE_ORD = 273.00f,
+            ORD_JFK = 641.00f,
+            DEN_JFK = 1409.00f,
+            JFK_SFO = 2242.00f,
+            JFK_MIA = 949.00f,
+            DFW_CYYZ = 1041.00f;
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Welcome to Zach Burkhardt Airlines" << endl;
     return 0;
 }
